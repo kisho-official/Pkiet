d6d295d34bc9599a3e169d6ae338c7172e4df284
unknown <758735@01HW195826.India.TCS.com>
2016-10-14 15:19:27 +0530

    placement authentication

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,48 @@
+class UsersController < ApplicationController
+
+  def index
+    @user = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])    
+    if @user.save
+      redirect_to root_url, :notice => 'User was successfully created.'
+    else
+      render :action => "new"
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+      if @user.update_attributes(params[:user])
+        redirect_to @user, :notice => 'User was successfully updated.'
+      else
+        render :action => "edit"
+      end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to users_url }
+      format.json { head :no_content }
+    end
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Placement::Application.config.secret_token = '3de3be228186505a3b26b4c412b9ded4766c8443e9365a14a84fdc47dd64133c317d5eda6771934164cf73ccdd8875f412c65013434ff5ef0c04ad54cf4f45aa'
